a065bf42de3660dd0cc5ece5ed72965c3f8a9d56
Demitrius Webb <101858569+Blacksanta10@users.noreply.github.com>
2022-04-22 22:44:28 -0400

    Add files via upload

---
+++ JAVA/Firstjava/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+//These make a sentence about my first code
+        System.out.print("I love programing, ");
+        
+        System.out.print("This is my first java code!");
+    }
+}

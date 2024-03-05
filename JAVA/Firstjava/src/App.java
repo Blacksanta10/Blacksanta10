fc85d20ca39591b0d797138895ecd77ef58ebb07
Demitrius Webb <101858569+Blacksanta10@users.noreply.github.com>
2024-03-05 00:08:10 -0500

    Delete JAVA/Firstjava directory

--- JAVA/Firstjava/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-//These make a sentence about my first code
-        System.out.print("I love programing, ");
-        
-        System.out.print("This is my first java code!");
-    }
-}
